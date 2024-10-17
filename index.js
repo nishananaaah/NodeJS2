b2ad46d6103b8a149f48759416898de79c35150e
nishananaaah <nishanaf731@gmail.com>
2024-10-17 15:02:25 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+var up =require('upper-case');
+var cl= require('cli-color')
+// console.log(up.upperCase("hello"))
+// console.log(cl.red("hello world"))
+console.log("hello world")
+console.log("hello nisana ")
